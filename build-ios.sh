24babbe3583b2c1070dab4b93428ef47961153ca
xiaorong.niu <xiaorong.niu@happyelements.com>
2023-05-06 12:18:24 +0800

    Add  build-its

---
+++ build-ios.sh
@@ -0,0 +1,16 @@
+LUAJIT=.
+DEVDIR=`xcode-select -print-path`
+IOSSDKVER=9.0
+IOSDIR=$DEVDIR/Platforms
+IOSBIN=$DEVDIR/Toolchains/XcodeDefault.xctoolchain/usr/bin
+
+
+
+ISDKF="-arch arm64 -isysroot $IOSDIR/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk -miphoneos-version-min=8.0"
+make HOST_CC="clang " CROSS=${IOSBIN}/ \
+STATIC_CC=${IOSBIN}/clang DYNAMIC_CC="${IOSBIN}/clang -fPIC" \
+TARGET_LD=${IOSBIN}/clang \
+TARGET_CFLAGS="${ISDKF}" \
+TARGET_SYS=iOS
+
+lipo   -arch arm64 src/libluajit.a  -create -output libluajit.a
