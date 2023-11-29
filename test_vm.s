8e60f007e4347d38327e7c933f6c4241d1ecf3cc
xiaorong.niu <xiaorong.niu@happyelements.com>
2023-11-29 14:31:24 +0800

    fix

---
+++ test_vm.s
@@ -0,0 +1,44 @@
+/*
+** This file has been pre-processed with DynASM.
+** https://luajit.org/dynasm.html
+** DynASM version 1.3.0, DynASM x64 version 1.3.0
+** DO NOT EDIT! The original file is in "test.dasc".
+*/
+
+#line 1 "test.dasc"
+//|.arch x64
+#if DASM_VERSION != 10300
+#error "Version mismatch between DynASM and included encoding engine"
+#endif
+#line 2 "test.dasc"
+//|.section code_op, code_sub
+#define DASM_SECTION_CODE_OP	0
+#define DASM_SECTION_CODE_SUB	1
+#define DASM_MAXSECTION		2
+#line 3 "test.dasc"
+//|.actionlist build_actionlist
+static const unsigned char build_actionlist[1] = {
+  255
+};
+
+#line 4 "test.dasc"
+//|.globals GLOB_
+enum {
+  GLOB__MAX
+};
+#line 5 "test.dasc"
+//|.globalnames globnames
+static const char *const globnames[] = {
+  (const char *)0
+};
+#line 6 "test.dasc"
+//|.externnames extnames
+static const char *const extnames[] = {
+  (const char *)0
+};
+#line 7 "test.dasc"
+
+
+static void test() {
+	
+}
