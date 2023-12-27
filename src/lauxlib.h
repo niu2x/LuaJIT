2475a8122afd6660d451e926f9994b98b8711f33
xiaorong.niu <xiaorong.niu@happyelements.com>
2023-12-27 11:11:52 +0800

    fix

--- src/lauxlib.h
+++ src/lauxlib.h
@@ -13,7 +13,6 @@
 #include <stdio.h>
 
 #include "lua.h"
-#define luaL_reg  luaL_Reg
 
 /* extra error code for `luaL_load' */
 #define LUA_ERRFILE     (LUA_ERRERR+1)
@@ -157,5 +156,25 @@ LUALIB_API void (luaL_pushresult) (luaL_Buffer *B);
 
 
 /* }====================================================== */
+
+
+
+/* compatibility with ref system */
+
+/* pre-defined references */
+#define LUA_NOREF       (-2)
+#define LUA_REFNIL      (-1)
+
+#define lua_ref(L,lock) ((lock) ? luaL_ref(L, LUA_REGISTRYINDEX) : \
+      (lua_pushstring(L, "unlocked references are obsolete"), lua_error(L), 0))
+
+#define lua_unref(L,ref)        luaL_unref(L, LUA_REGISTRYINDEX, (ref))
+
+#define lua_getref(L,ref)       lua_rawgeti(L, LUA_REGISTRYINDEX, (ref))
+
+
+#define luaL_reg  luaL_Reg
+
+
 
 #endif
